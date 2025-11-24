55a13917936942eebfc6b47456611e8e436f8ae9
alexandre <alexandre020602@gmail.com>
2025-11-24 00:36:44 -0300

    f

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,7 @@ import { Toaster } from 'react-hot-toast'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'https://thinkboard-h2sl.onrender.com/api'
+axios.defaults.withCredentials = true
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
