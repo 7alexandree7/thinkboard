b500d21f0818a140f01a9dd35897579e96a650ae
alexandre <alexandre020602@gmail.com>
2025-11-24 00:08:29 -0300

    a

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,9 +3,10 @@ import cors from "cors";
 import notesRoutes from "./routes/notesRoutes";
 import db from "./db/config";
 import rateLimiter from "./middlewares/rateLimiter";
+import 'dotenv/config'
 
 const app: express.Application = express();
-const PORT: number | string = 3000;
+const PORT =  process.env.PORT || 3000
 
 app.use(cors({
     origin: 'http://localhost:5173'
